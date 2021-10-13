95fe6b4fd6cdf980361a8ab0d5f4b72aa5b8c55f
PabloHdzVizcarra <pablourbanohdzvizcarra@gmail.com>
2021-10-13 18:21:44 -0500

    feat template render and add data model

--- src/main/java/jvm/pablo/webcrawler/controller/CrawlerController.java
+++ src/main/java/jvm/pablo/webcrawler/controller/CrawlerController.java
@@ -27,4 +27,5 @@ public class CrawlerController {
         List<String> urls = crawlerService.findUrls(url);
         return ResponseEntity.ok(urls);
     }
+
 }
--- src/main/java/jvm/pablo/webcrawler/controller/TemplateController.java
+++ src/main/java/jvm/pablo/webcrawler/controller/TemplateController.java
@@ -1,13 +1,28 @@
 package jvm.pablo.webcrawler.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import jvm.pablo.webcrawler.model.UrlRequest;
 
 @Controller
 public class TemplateController {
 
     @RequestMapping("/")
-    public String index() {
+    public String index(Model model) {
+        model.addAttribute("request", new UrlRequest());
+        model.addAttribute("exampleValue", "simple text");
         return "index";
     }
+
+    @PostMapping("/crawler")
+    public String findByUrl(@ModelAttribute UrlRequest data, Model model) {
+        model.addAttribute("modelValue", "Tom Clancy's");
+        model.addAttribute("request", new UrlRequest());
+        return "index";
+    }
+
 }
