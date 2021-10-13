8e3c7df667708ad09ee1df402849b6c2e36df5db
PabloHdzVizcarra <pablourbanohdzvizcarra@gmail.com>
2021-10-13 18:29:04 -0500

    feat controller can get urls to request data

--- src/main/java/jvm/pablo/webcrawler/controller/TemplateController.java
+++ src/main/java/jvm/pablo/webcrawler/controller/TemplateController.java
@@ -1,26 +1,37 @@
 package jvm.pablo.webcrawler.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.List;
+
 import jvm.pablo.webcrawler.model.UrlRequest;
+import jvm.pablo.webcrawler.service.CrawlerService;
 
 @Controller
 public class TemplateController {
+    private final CrawlerService crawlerService;
+
+    @Autowired
+    public TemplateController(CrawlerService crawlerService) {
+        this.crawlerService = crawlerService;
+    }
 
     @RequestMapping("/")
     public String index(Model model) {
         model.addAttribute("request", new UrlRequest());
-        model.addAttribute("exampleValue", "simple text");
+
         return "index";
     }
 
     @PostMapping("/crawler")
     public String findByUrl(@ModelAttribute UrlRequest data, Model model) {
-        model.addAttribute("modelValue", "Tom Clancy's");
+        List<String> urls = crawlerService.findUrls(data.getUrl());
+
         model.addAttribute("request", new UrlRequest());
         return "index";
     }
